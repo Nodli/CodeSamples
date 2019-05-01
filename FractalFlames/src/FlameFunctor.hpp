4c43d9721e3da40ef8d808b6766ce96bf1afa799
Nodli <rambure.hugo@hotmail.fr>
2019-05-01 08:51:48 +0200

    Improve structure

--- FractalFlames/src/FlameFunctor.hpp
+++ FractalFlames/src/FlameFunctor.hpp
@@ -11,77 +11,90 @@ int rand_int(const int min, const int max);
 float rand_float(const float min, const float max);
 int binary_search(const std::vector<float>& cumulated_values, float random_value);
 
-namespace IFS{
-	struct Function{
-		float a;
-		float b;
-		float c;
-		float d;
-		float e;
-		float f;
+struct Function{
+	float a;
+	float b;
+	float c;
+	float d;
+	float e;
+	float f;
 
-		void operator()(float& x, float& y) const;
-	};
+	void operator()(float& x, float& y) const;
+};
 
-	namespace Library{
-		// std::vector<FunctionType>
-		//		functions with uniform probabilities
-		std::vector<Function> serpinski_gasket();
-		std::vector<Function> levy_curve();
-		std::vector<Function> heighway_dragon_cartesian();
+struct Variation{
+	std::vector<float> param;
 
-		// std::vector<std::pair<Function, float>>
-		//		functions with provided non-uniform probabilities
-		std::vector<std::pair<Function, float>> barnsley_fern();
-		std::vector<std::pair<Function, float>> barnsley_fern_mutated();
+	void operator()(float& x, float& y) const;
+};
 
-		// std::pair<std::vector<Function>, std::vector<std::pair<float, float>>>
-		//		functions with uniform probabilities and a provided initial sample set
-		std::pair<std::vector<Function>, std::vector<std::pair<float, float>>> heighway_dragon_complex();
-		std::pair<std::vector<Function>, std::vector<std::pair<float, float>>> twin_dragon_complex();
-		std::pair<std::vector<Function>, std::vector<std::pair<float, float>>> ter_dragon_complex();
-	}
+namespace Variation{
+	void V0(float& x, float& y);
+	void V1(float& x, float& y);
+	void V2(float& x, float& y);
+	void V3(float& x, float& y);
+	void V4(float& x, float& y);
+}
 
-	// runs a chaos game with a set of functions with uniform probabilities
-	// returns a view of the density of points in a bi-unit square scaled by
-	// /scale/ and centered at (/center_x/, /center_y/)
-	template<typename FunctionType>
-	Buffer2D<unsigned int> solve(const std::vector<FunctionType>& func,
-			const unsigned int pixel_resolution,
-			const unsigned int sample_per_pixel,
-			const unsigned int iterations_per_sample = 20,
-			const float center_x = 0.f, const float center_y = 0.f,
-			const float scale = 1.f);
+namespace Library{
+	// std::vector<FunctionType>
+	//		functions with uniform probabilities
+	std::vector<Function> serpinski_gasket();
+	std::vector<Function> levy_curve();
+	std::vector<Function> heighway_dragon_cartesian();
 
-	// runs a chaos game with a set of functions with different probabilities
-	// returns a view of the density of points in a bi-unit square scaled by
-	// /scale/ and centered at (/center_x/, /center_y/)
-	template<typename FunctionType>
-	Buffer2D<unsigned int> solve(const std::vector<std::pair<FunctionType, float>>& func_prob,
-			const unsigned int pixel_resolution,
-			const unsigned int sample_per_pixel,
-			const unsigned int iterations_per_sample = 20,
-			const float center_x = 0.f, const float center_y = 0.f,
-			const float scale = 1.f);
+	// std::vector<std::pair<Function, float>>
+	//		functions with provided non-uniform probabilities
+	std::vector<std::pair<Function, float>> barnsley_fern();
+	std::vector<std::pair<Function, float>> barnsley_fern_mutated();
 
-	// runs a chaos game with a set of functions with uniform probabilities
-	// with the provided starting coordinates
-	// returns a view of the density of points in a bi-unit square scaled by
-	// 	/scale/ and centedred at (/center_x/, /center_y/)
-	template<typename FunctionType>
-	Buffer2D<unsigned int> solve_complex(
+	// std::pair<std::vector<Function>, std::vector<std::pair<float, float>>>
+	//		functions with uniform probabilities and a provided initial sample set
+	std::pair<std::vector<Function>, std::vector<std::pair<float, float>>> heighway_dragon_complex();
+	std::pair<std::vector<Function>, std::vector<std::pair<float, float>>> twin_dragon_complex();
+	std::pair<std::vector<Function>, std::vector<std::pair<float, float>>> ter_dragon_complex();
+	std::pair<std::vector<Function>, std::vector<std::pair<float, float>>> tree_complex();
+}
+
+// runs a chaos game with a set of functions with uniform probabilities
+// returns a view of the density of points in a bi-unit square scaled by
+// /scale/ and centered at (/center_x/, /center_y/)
+template<typename FunctionType>
+Buffer2D<unsigned int> solve(const std::vector<FunctionType>& func,
+		const unsigned int pixel_resolution,
+		const unsigned int sample_per_pixel,
+		const unsigned int iterations_per_sample = 20,
+		const float center_x = 0.f, const float center_y = 0.f,
+		const float scale = 1.f);
+
+// runs a chaos game with a set of functions with different probabilities
+// returns a view of the density of points in a bi-unit square scaled by
+// /scale/ and centered at (/center_x/, /center_y/)
+template<typename FunctionType>
+Buffer2D<unsigned int> solve(const std::vector<std::pair<FunctionType, float>>& func_prob,
+		const unsigned int pixel_resolution,
+		const unsigned int sample_per_pixel,
+		const unsigned int iterations_per_sample = 20,
+		const float center_x = 0.f, const float center_y = 0.f,
+		const float scale = 1.f);
+
+// runs a chaos game with a set of functions with uniform probabilities
+// with the provided starting coordinates
+// returns a view of the density of points in a bi-unit square scaled by
+// 	/scale/ and centedred at (/center_x/, /center_y/)
+template<typename FunctionType>
+Buffer2D<unsigned int> solve_complex(
 		const std::pair<std::vector<FunctionType>, std::vector<std::pair<float, float>>>& func_sample,
 		const unsigned int pixel_resolution,
 		const unsigned int iterations_per_sample = 20,
 		const float center_x = 0.f, const float center_y = 0.f,
 		const float scale = 1.f);
 
-	Buffer2D<RGB> render(const Buffer2D<unsigned int>& src,
-			const ColorMap& colormap = Palette::viridis());
-	Buffer2D<RGB> binary_render(const Buffer2D<unsigned int>& src);
-	Buffer2D<RGB> log_render(const Buffer2D<unsigned int>& src,
-			const ColorMap& colormap = Palette::viridis());
-}
+Buffer2D<RGB> render(const Buffer2D<unsigned int>& src,
+		const ColorMap& colormap = Palette::viridis());
+Buffer2D<RGB> binary_render(const Buffer2D<unsigned int>& src);
+Buffer2D<RGB> log_render(const Buffer2D<unsigned int>& src,
+		const ColorMap& colormap = Palette::viridis());
 
 #include "FlameFunctor.inl"
 
--- FractalFlames/src/main.cpp
+++ FractalFlames/src/main.cpp
@@ -4,20 +4,20 @@
 
 int main(){
 
-	//Buffer2D<unsigned int> solution = IFS::solve(IFS::Library::serpinski_gasket(), 500, 10, 20, 0.5f, 0.5f, 1.f);
-	//Buffer2D<unsigned int> solution = IFS::solve(IFS::Library::levy_curve(), 500, 10, 20, 0.5f, 0.5f, 2.f);
-	//Buffer2D<unsigned int> solution = IFS::solve(IFS::Library::heighway_dragon_cartesian(), 500, 10, 20, 0.5f, 0.f, 2.f);
+	//Buffer2D<unsigned int> solution = solve(Library::serpinski_gasket(), 500, 10, 20, 0.5f, 0.5f, 1.f);
+	//Buffer2D<unsigned int> solution = solve(Library::levy_curve(), 500, 10, 20, 0.5f, 0.5f, 2.f);
+	//Buffer2D<unsigned int> solution = solve(Library::heighway_dragon_cartesian(), 500, 10, 20, 0.5f, 0.f, 2.f);
 
-	//Buffer2D<unsigned int> solution = IFS::solve(IFS::Library::barnsley_fern(), 500, 10, 20, 7.5f, 9.f, 10.f);
-	//Buffer2D<unsigned int> solution = IFS::solve(IFS::Library::barnsley_fern_mutated(), 500, 10, 20, 7.5f, 9.f, 10.f);
+	//Buffer2D<unsigned int> solution = solve(Library::barnsley_fern(), 500, 10, 20, 7.5f, 9.f, 10.f);
+	//Buffer2D<unsigned int> solution = solve(Library::barnsley_fern_mutated(), 500, 10, 20, 7.5f, 9.f, 10.f);
 
-	//Buffer2D<unsigned int> solution = IFS::solve_complex(IFS::Library::heighway_dragon_complex(), 500, 20, 0.4f, 0.2f, 1.f);
-	//Buffer2D<unsigned int> solution = IFS::solve_complex(IFS::Library::twin_dragon_complex(), 500, 20, 0.5f, 0.f, 1.f);
-	Buffer2D<unsigned int> solution = IFS::solve_complex(IFS::Library::ter_dragon_complex(), 500, 15, 0.5f, 0.f, 1.f);
+	//Buffer2D<unsigned int> solution = solve_complex(Library::heighway_dragon_complex(), 500, 20, 0.4f, 0.2f, 1.f);
+	//Buffer2D<unsigned int> solution = solve_complex(Library::twin_dragon_complex(), 500, 20, 0.5f, 0.f, 1.f);
+	Buffer2D<unsigned int> solution = solve_complex(Library::ter_dragon_complex(), 500, 15, 0.5f, 0.f, 1.f);
 
-	Buffer2D<RGB> image = IFS::render(solution);
-	//Buffer2D<RGB> image = IFS::binary_render(solution);
-	//Buffer2D<RGB> image = IFS::log_render(solution);
+	Buffer2D<RGB> image = render(solution);
+	//Buffer2D<RGB> image = binary_render(solution);
+	//Buffer2D<RGB> image = log_render(solution);
 
 	//writeP3("serpinski_gasket.ppm", image.sizeX, image.sizeY, (unsigned char*)(image.data));
 	//writeP3("levy_curve.ppm", image.sizeX, image.sizeY, (unsigned char*)(image.data));
