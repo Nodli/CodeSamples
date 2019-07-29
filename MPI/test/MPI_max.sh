c98db584e46d07ae83f7e7d7d0915d5653e73350
Nodli <rambure.hugo@hotmail.fr>
2019-07-29 13:33:49 +0200

    Update test.sh with relative paths

--- MPI/test/MPI_max.sh
+++ MPI/test/MPI_max.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p build
-mpicc MPI_max.c -lm -o build/max.exe
+mpicc ../MPI_max.c -lm -o build/max.exe
 cd build
 mpiexec -n 4 ./max.exe 99
--- MPI/test/MPI_pi.sh
+++ MPI/test/MPI_pi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p build
-mpicc MPI_pi.c -o build/pi.exe
+mpicc ../MPI_pi.c -o build/pi.exe
 cd build
 mpiexec -n 4 ./pi.exe 100000
